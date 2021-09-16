062fd980fd4b525ae34afe72b04d9a2ba7510ad2
erin <erinverkoulen93@gmail.com>
2021-09-15 23:10:51 -0500

    commit tree 24c95a327a1d18b250de7ca91b1ab3119d68d21b

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+
+
+
+
+# :: refers to namespaced modules or classes, references the parent and child relationship of the nested modules
